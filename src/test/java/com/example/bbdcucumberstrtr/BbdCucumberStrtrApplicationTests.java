0d673cd94211133e2f7adba4f01476add99405a1
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2024-04-26 14:05:56 +0530

    Initial Setup

---
+++ src/test/java/com/example/bbdcucumberstrtr/BbdCucumberStrtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbdcucumberstrtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbdCucumberStrtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
